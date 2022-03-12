e1ca9b55b59c003a031102597e2faf4963285f74
hatter <zaeem.z@rutgers.edu>
2022-03-12 01:53:09 -0500

    requirements.txt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 # Roblox Limiteds Sniper
-import configparser, time, warnings, os, requests, cloudscraper, cfscrape, bs4
-from typing import Counter
-from logging import error
-from bs4 import BeautifulSoup
+import configparser, time, warnings, os
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
